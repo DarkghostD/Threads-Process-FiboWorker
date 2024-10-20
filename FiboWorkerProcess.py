ca3b40b00c75c63739341899281b05e0bb57f273
SergioART2023 <145519325+SergioART2023@users.noreply.github.com>
2024-10-19 21:10:38 -0500

    FiboWorkerProcess.py

--- FiboWorkerProcess.py
+++ FiboWorkerProcess.py
@@ -8,40 +8,40 @@ def fibo(n):
     return fibo(n-1) + fibo(n-2)
 
 class FiboWorker(multiprocessing.Process):
-    def __init__(self, vector, start_index, end_index, result_queue):
+    def __init__(self, vector, startIndex, endIndex, result_queue):
         multiprocessing.Process.__init__(self)
         self.vector = vector
-        self.start_index = start_index
-        self.end_index = end_index
+        self.startIndex = startIndex
+        self.endIndex = endIndex
         self.result_queue = result_queue
 
     def run(self):
-        for i in range(self.start_index, self.end_index):
+        for i in range(self.startIndex, self.endIndex):
             self.vector[i] = fibo(self.vector[i])
-        self.result_queue.put((self.start_index, self.end_index, self.vector[self.start_index:self.end_index]))
+        self.result_queue.put((self.startIndex, self.endIndex, self.vector[self.startIndex:self.endIndex]))
 
 def main():
-    vector_length = 144
+    vectorLenght = 144
     initial_value = 33
     num_cpus = multiprocessing.cpu_count()
 
     # Inicializar el vector
-    vector = np.full(vector_length, initial_value)
+    vector = np.full(vectorLenght, initial_value)
 
-    print(f"Procesando un vector de longitud {vector_length} con valor inicial {initial_value}")
+    print(f"Procesando un vector de longitud {vectorLenght} con valor inicial {initial_value}")
     print(f"Utilizando {num_cpus} CPUs")
 
     # Dividir el trabajo entre los CPUs disponibles
-    chunk_size = vector_length // num_cpus
+    chunk_size = vectorLenght // num_cpus
     processes = []
     result_queue = multiprocessing.Queue()
 
     ts = time()
 
     for i in range(num_cpus):
-        start_index = i * chunk_size
-        end_index = start_index + chunk_size if i < num_cpus - 1 else vector_length
-        worker = FiboWorker(vector, start_index, end_index, result_queue)
+        startIndex = i * chunk_size
+        endIndex = startIndex + chunk_size if i < num_cpus - 1 else vectorLenght
+        worker = FiboWorker(vector, startIndex, endIndex, result_queue)
         processes.append(worker)
         worker.start()
 
